593dfbf26329a478816a4523611d195a05aaf35f
Paulo Cesar Jesuino Filho <110634982+Paulocjf@users.noreply.github.com>
2026-04-24 20:57:25 -0300

    Add program to display integers from 50 to 200

---
+++ Algoritmos 1/exercícios/repeticao/ex03.cpp
@@ -0,0 +1,16 @@
+// Faça um programa que exiba na tela os números inteiros de 50 até 200.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    int contador = 50;
+    
+    while (contador <= 200) {
+        cout << contador << endl;
+        contador++;
+    }
+    
+    return 0;
+}
