ca27728eee007abcf1d665163c80a9a1f5c45af6
Paulo Cesar Jesuino Filho <110634982+Paulocjf@users.noreply.github.com>
2026-04-24 21:10:16 -0300

    Implement program to display integers from 100 to 1

--- Algoritmos 1/exercícios/repeticao/ex03.cpp
+++ Algoritmos 1/exercícios/repeticao/ex03.cpp
@@ -1,1 +1,16 @@
+// Faça um programa que exiba na tela os números inteiros de 100 até 1.
 
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    int contador = 100;
+    
+    while (contador >= 1) {
+        cout << contador << endl;
+        contador--;
+    }
+    
+    return 0;
+}
